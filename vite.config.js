88d506ef5a7eed3d5b97dd9d85082f749d71bcc8
dlatkdgns456-ship-it <dlatkdgns456@gmail.com>
2026-07-24 06:36:50 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/NEWSROOM-AI-DEMO/',
+  base: '/NUSDEMO/',
   plugins: [react()],
 })
